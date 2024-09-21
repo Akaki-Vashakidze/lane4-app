6bd103ad5aa5b93237c70788be9cfbc171107f60
Ako <akaki.vashakidze1@gmail.com>
2024-09-21 13:45:47 +0400

    header done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatIconModule,MatButtonModule],
+  imports: [RouterOutlet,MatIconModule,MatButtonModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'lane4-console';
+  navLeftItems = ['Home', 'Categories'];
+  navRightItems = ['Offers', 'Contact', 'About Us'];
+  buttonLabel = 'Register';
 }
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Input() navLeftItems: string[] = [];
+  @Input() navRightItems: string[] = [];
+  @Input() buttonLabel: string = 'Button'; // This can also come dynamically
+
+}

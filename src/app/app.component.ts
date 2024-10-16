da67890719479cd23f676b595b2218d55bba05d3
Ako <akaki.vashakidze1@gmail.com>
2024-10-16 21:52:16 +0400

    adding banner card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './layout/header/header.component';
+import { BannerCardComponent } from './shared/components/banner-card/banner-card.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatIconModule,MatButtonModule,HeaderComponent],
+  imports: [RouterOutlet,MatIconModule,MatButtonModule,HeaderComponent, BannerCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

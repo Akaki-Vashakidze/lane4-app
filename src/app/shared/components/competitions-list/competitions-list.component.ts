7ba8c3e4c3ece0076923ed585e86645596ba90b0
Ako <akaki.vashakidze1@gmail.com>
2024-10-30 14:44:36 +0400

    changing img

--- src/app/shared/components/competitions-list/competitions-list.component.ts
+++ src/app/shared/components/competitions-list/competitions-list.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CompetitionCardComponent } from '../competition-card/competition-card.component';
 import { ButtonComponent } from '../button/button.component';
 import { V } from '@angular/cdk/keycodes';
 import { SharedService } from '../../services/shared.service';
 import { LiveEvent } from '../../interfaces/interfaces';
 import { LoaderSpinnerComponent } from '../loader-spinner/loader-spinner.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-competitions-list',
   standalone: true,
-  imports: [CommonModule,CompetitionCardComponent,ButtonComponent, LoaderSpinnerComponent],
+  imports: [CommonModule,CompetitionCardComponent,ButtonComponent, LoaderSpinnerComponent, TranslateModule],
   templateUrl: './competitions-list.component.html',
   styleUrl: './competitions-list.component.scss'
 })
 export class CompetitionsListComponent {
+  @Input() BackgroundImg: boolean = false;
   constructor(private sharedService:SharedService){
     sharedService.getCompetitions().subscribe(item => {
       this.competitions = [...item,...item,...item,...item,...item];

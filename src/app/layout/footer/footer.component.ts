527e2776a1663f1a4afc88f84f1265ccdebb9d1d
Ako <akaki.vashakidze1@gmail.com>
2024-11-05 16:13:49 +0400

    making footer responcive

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { InputComponent } from '../../shared/components/input/input.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule,TranslateModule,RouterModule],
+  imports: [CommonModule,TranslateModule,RouterModule,InputComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })

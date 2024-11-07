9ec654d49effda3dc71d6dd455711a3c3431aa48
Ako <akaki.vashakidze1@gmail.com>
2024-11-07 19:16:17 +0400

    tabs double

---
+++ src/app/shared/components/double-custom-tabs/double-custom-tabs.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-double-custom-tabs',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './double-custom-tabs.component.html',
+  styleUrl: './double-custom-tabs.component.scss'
+})
+export class DoubleCustomTabsComponent {
+  @Input() tabs1:string[] = ['tab1','tab2'];
+  @Input() tabs2:string[] = ['tab3','tab4'];
+  @Input() header!:string;
+  @Input() tabsWidth:string = '94%';
+  @Input() infoTitle!:string;
+  @Input() infoText!:string;
+  active1TabIndex:number = 0; 
+  active2TabIndex:number = 0; 
+  @Output() tabChanged1 = new EventEmitter<number>();
+  @Output() tabChanged2 = new EventEmitter<number>();
+  tabChange1(index:number){
+   this.active1TabIndex = index;
+   this.tabChanged1.emit(index); 
+  }
+  tabChange2(index:number){
+    this.active2TabIndex = index;
+    this.tabChanged2.emit(index); 
+   }
+}
--- src/app/shared/components/records/records.component.ts
+++ src/app/shared/components/records/records.component.ts
@@ -5,16 +5,18 @@ import { CustomTabsComponent } from '../custom-tabs/custom-tabs.component';
 import { SharedService } from '../../services/shared.service';
 import { LoaderSpinnerComponent } from '../loader-spinner/loader-spinner.component';
 import { TimeComponent } from '../time/time.component';
+import { DoubleCustomTabsComponent } from '../double-custom-tabs/double-custom-tabs.component';
 
 @Component({
   selector: 'app-records',
   standalone: true,
-  imports: [CommonModule, TranslateModule,CustomTabsComponent,LoaderSpinnerComponent,TimeComponent],
+  imports: [CommonModule, TranslateModule,DoubleCustomTabsComponent,LoaderSpinnerComponent,TimeComponent],
   templateUrl: './records.component.html',
   styleUrl: './records.component.scss'
 })
 export class RecordsComponent {
-  tabs = ["50m Men","50m Women"]
+  tabs1 = ["Long_Course","Short_Course"]
+  tabs2 = ["Men","Women"]
   chosenRecords = signal<any>([])
   constructor(public _sharedService:SharedService){
     _sharedService.getWRTs().subscribe(item => {
@@ -22,6 +24,8 @@ export class RecordsComponent {
     })
   }
   array: any = []
+  tab1Index:number = 0;
+  tab2Index:number = 0;
   transformRanksToArray(rankings: any) {
     const tables: any = [];
     let course!:string;
@@ -51,7 +55,27 @@ export class RecordsComponent {
     return this.array;
   }
 
-  onTabChange(index:number){
-   index == 0 ? this.chosenRecords.set(this.array[1]) : this.chosenRecords.set(this.array[3]);
+  chooseRecordsArray(){
+    if(this.tab1Index == 0 && this.tab2Index == 0){
+      this.chosenRecords.set(this.array[1])
+    } else if(this.tab1Index == 1 && this.tab2Index == 0){
+      this.chosenRecords.set(this.array[0])
+    } else if(this.tab1Index == 0 && this.tab2Index == 1){
+      this.chosenRecords.set(this.array[3])
+    } else if(this.tab1Index == 1 && this.tab2Index == 1){
+      this.chosenRecords.set(this.array[2])
+    }
+    console.log(this.chosenRecords())
   }
+
+  on1TabChange(index:number){
+    this.tab1Index = index;
+    this.chooseRecordsArray()
+  }
+
+  on2TabChange(index:number){
+    console.log(index)
+    this.tab2Index = index;
+    this.chooseRecordsArray()
+   }
 }

9ef7be7a8f1c18c3b6578052d79301234e3d91ea
Ako <akaki.vashakidze1@gmail.com>
2024-11-11 19:39:36 +0400

    athlete page

--- src/app/shared/components/athlete-results/athlete-results.component.ts
+++ src/app/shared/components/athlete-results/athlete-results.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { SelectComponent } from '../select/select.component';
 
 @Component({
   selector: 'app-athlete-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule,SelectComponent],
   templateUrl: './athlete-results.component.html',
   styleUrl: './athlete-results.component.scss'
 })
 export class AthleteResultsComponent {
+  currentYear: number;
+  yearsForSelect:any[] = []
   constructor(public _sharedService:SharedService){
-    //66479f1adc0fa3765b1287c6
-
-    //666b3c0bcdd10df634af8070
+    this.currentYear = new Date().getFullYear();
+    this.populateYearsForSelect();
     _sharedService.getAthleteResults('666b3c0bcdd10df634af8070').subscribe(item => console.log(item))
+  }
+
+  onSelect1(event:any){
+    console.log(event)
+  }
+
+  onSelect2(event:any){
+    console.log(event)
+  }
+
+  populateYearsForSelect() {
+    for (let year = 2005; year <= this.currentYear; year++) {
+      this.yearsForSelect.push(year);
+    }
   }
 }
---
+++ src/app/shared/components/select/select.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss'
+})
+export class SelectComponent {
+  @Input() options: string[] = ['Alltime', 'Today', 'This Week', 'This Month'];
+  selectedOption: string = 'Alltime';
+  isDropdownOpen: boolean = false;
+  @Input() width:string = '120px'
+  @Input() height:string = '400px'
+  @Output() itemSelected = new EventEmitter<string>();
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isDropdownOpen = false;
+    this.itemSelected.emit(option); 
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.custom-dropdown')) {
+      this.isDropdownOpen = false;
+    }
+  }
+}

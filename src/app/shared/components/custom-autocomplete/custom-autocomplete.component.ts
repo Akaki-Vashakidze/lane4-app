42f11093788ab5acbe6eaa4b4b18717f0fc1007e
Ako <akaki.vashakidze1@gmail.com>
2024-11-13 13:39:40 +0400

    env

--- src/app/shared/components/custom-autocomplete/custom-autocomplete.component.ts
+++ src/app/shared/components/custom-autocomplete/custom-autocomplete.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -8,26 +8,34 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [CommonModule, MatIconModule, FormsModule],
   templateUrl: './custom-autocomplete.component.html',
-  styleUrl: './custom-autocomplete.component.scss'
+  styleUrls: ['./custom-autocomplete.component.scss']
 })
 export class CustomAutocompleteComponent {
-  @Input() data!:any[]
+  @Input() data!: any[];
   @Output() onSelect = new EventEmitter<any>();
   searchQuery: string = '';
   filteredSuggestions: any[] = [];
-  chosenOption!:any;
+  chosenOption!: any;
 
   onInput(event: Event) {
     const query = this.searchQuery.toLowerCase();
-    this.filteredSuggestions = this.data.filter(item => {
-      return  item.lastName.toLowerCase().includes(query) || item.firstName.toLowerCase().includes(query)
-    });
+    this.filteredSuggestions = this.data.filter(item => 
+      item.lastName.toLowerCase().includes(query) || item.firstName.toLowerCase().includes(query)
+    );
   }
 
   selectSuggestion(suggestion: any) {
-    this.searchQuery = suggestion.lastName + ' ' + suggestion.firstName;
+    this.searchQuery = `${suggestion.lastName} ${suggestion.firstName}`;
     this.chosenOption = suggestion;
     this.onSelect.emit(this.chosenOption); 
     this.filteredSuggestions = [];
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClickOutside(targetElement: HTMLElement) {
+    const clickedInside = targetElement.closest('.input-wrapper') || targetElement.closest('.suggestions-list');
+    if (!clickedInside) {
+      this.filteredSuggestions = [];
+    }
   }
 }

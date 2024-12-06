d32d6e90edc434840b2b136b577adf306b1fe9da
Ako <akaki.vashakidze1@gmail.com>
2024-12-06 18:07:31 +0400

    changes

--- src/app/features/competitions/services/competition.service.ts
+++ src/app/features/competitions/services/competition.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { EventDetails, Time } from '../../../shared/interfaces/interfaces';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,16 @@ export class CompetitionService {
 
   convertResultToSeconds(result:Time) {
     return (result?.minutes * 60) + (result?.seconds * 1) + (result?.milliseconds / 100)
+  }
+
+  getPlannedEvents(){
+    return this.httpClient.get<any[]>(`/consoleApi/event/planned`);
+  }
+
+  getAllCompetitions() {
+    return this.httpClient
+      .get<Event[]>(`/consoleApi/event/all`).pipe(
+        tap(item => console.log(item))
+      )
   }
 }

678adaa658f64ec02de1cb765ebc2b316d5ded44
Ako <akaki.vashakidze1@gmail.com>
2026-05-21 10:45:55 +0400

    translates backup

--- src/app/features/competitions/components/start-list/start-list.component.ts
+++ src/app/features/competitions/components/start-list/start-list.component.ts
@@ -59,7 +59,7 @@ export class StartListComponent {
       .pipe(takeUntilDestroyed())
       .subscribe(lang => {
         this.lang.set(lang || 'en')
-        this.partitionTitles = this.partitions?.map(item => (this.lang() === 'ka' ? item.title : item?.translations?.en?.title))
+        this.partitionTitles = this.partitions?.map(item => (this.lang() === 'ka' ? item.title : item?.translations?.en?.title ?? item.title))
       }
     );
 
@@ -73,7 +73,7 @@ export class StartListComponent {
       });
       this.chosenPartition.set(this.partitions[0])
       this.chosenPartition().races.sort((a: any, b: any) => a.orderNumber - b.orderNumber);
-      this.partitionTitles = this.partitions.map(item => (this.lang() === 'ka' ? item.title : item?.translations?.en?.title))
+      this.partitionTitles = this.partitions.map(item => (this.lang() === 'ka' ? item.title : item?.translations?.en?.title ?? item.title))
     })
 
   }
